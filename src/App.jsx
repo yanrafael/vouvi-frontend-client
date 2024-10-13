fefa072829d5f6dbd80300c8056bdabae893df2c
Marcos <msmarcondes10@gmail.com>
2024-10-13 20:29:03 -0300

    Atualiza App.jsx para o formato utilizando Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import HeaderWhite from "./components/Header/HeaderWhite";
-import Footer from "./components/Footer/Footer";
 import Home from './components/pages/Home/Home'
 import Login from './components/pages/Login/Login'
-// import Profile from './components/pages/profile/Profile'
 
 import { Route, Routes } from "react-router-dom";
 function App() {
 
   return (
     <>
-      <HeaderWhite />
       <Routes>
         <Route Component={Home} path="/" />
         <Route Component={Login} path="/login" />
