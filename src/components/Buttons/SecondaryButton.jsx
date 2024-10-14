f33e606fe4f88f4d95d73b65ecab5bb099c40359
wesley <wesley.rochao@hotmail.com>
2024-10-14 13:22:16 -0300

    SecondaryButton, style props

--- src/components/Buttons/SecondaryButton.jsx
+++ src/components/Buttons/SecondaryButton.jsx
@@ -1,6 +1,6 @@
-function SecondaryButton({text}){
+function SecondaryButton({text,color}){
     return(
-        <button className="bg-white  h-[75px] px-10 text-[32px] rounded-md flex font-bold items-center justify-center ">
+        <button className={`${color} bg-white  h-[75px] px-10 text-[32px] rounded-md flex font-bold items-center justify-center`}>
             <p>{text}</p>
         </button>
     )
