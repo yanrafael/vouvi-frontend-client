95836502569c1e4ac61c84a096ad164bface28a1
Marcos <msmarcondes10@gmail.com>
2024-10-22 15:18:16 -0300

    Finaliza o cadastro

--- src/components/pages/CreateAccount/CreateAccountStep5.jsx
+++ src/components/pages/CreateAccount/CreateAccountStep5.jsx
@@ -2,6 +2,17 @@ import ProgressBar from "../../Forms/ProgressBar";
 import SubmitButton from "../../Forms/SubmitButton";
 
 function CreateAccountStep5() {
+  // const inputs = document.querySelectorAll(".inputs");
+  // const container = document.getElementById('input-container');
+
+    const changeToNext = (e) => {
+      const nextInput = e.target.nextElementSibling;
+
+        if (nextInput && e.value !== "") {
+          console.log(nextInput);
+        }
+    }
+
   return (
     <>
       <ProgressBar value={60} />
@@ -9,24 +20,27 @@ function CreateAccountStep5() {
       {/* Área onde o usuário insere informações */}
       <div>
         <h2>Agora é só confirmar o seu e-mail</h2>
-        <div className="flex justify-center items-center gap-4">
+        <div id="input-container" className="flex justify-center items-center gap-4">
           <input
             className="w-20 h-20 text-base text-center border-none rounded-md font-bold cursor-pointer"
             type="text"
             maxLength={1}
             required
+            onKeyUp={changeToNext}
           />
           <input
             className="w-20 h-20 text-base text-center border-none rounded-md font-bold cursor-pointer"
             type="text"
             maxLength={1}
             required
+            onKeyUp={changeToNext}
           />
           <input
             className="w-20 h-20 text-base text-center border-none rounded-md font-bold cursor-pointer"
             type="text"
             maxLength={1}
             required
+            onKeyUp={changeToNext}
           />
           <input
             className="w-20 h-20 text-base text-center border-none rounded-md font-bold cursor-pointer"
