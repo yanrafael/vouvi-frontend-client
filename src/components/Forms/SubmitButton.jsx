f91b27d3d6649cffa2b843ed34143fc00a8c677a
Marcos <msmarcondes10@gmail.com>
2024-10-22 09:08:55 -0300

    Cria novo componente para botão de enviar

---
+++ src/components/Forms/SubmitButton.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+function SubmitButton({ text, onChange }) {
+  return (
+    <input
+      className="w-full bg-primary-200 hover:bg-secondary-200 focus:bg-secondary-200 text-white hover:text-secondary-300 focus:text-secondary-300 transition-all hover:cursor-pointer py-4 rounded-md text-base font-bold"
+      id="next-button"
+      type="button"
+      value={text}
+      onClick={onChange}
+    />
+  );
+}
+
+SubmitButton.propTypes = {
+  text: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default SubmitButton;
