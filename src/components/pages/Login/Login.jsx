640e136f66a7bc64252802b60f567a2a1fde79d7
Marcos <msmarcondes10@gmail.com>
2024-10-23 10:09:28 -0300

    Arruma estilo da página de login

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -4,38 +4,66 @@ import PasswordInput from "../../Forms/PasswordInput";
 import KeepConected from "../../Forms/Radio";
 
 function Form() {
-    return (
-        <div className="flex justify-center items-center sm:h-[850px] md:h-[750px] m-5">
-            <form className="flex flex-col justify-around gap-4 bg-white bg-opacity-90 w-[40%] h-full rounded-sm p-10" action="">
-                <h1 className="text-lg text-center font-bold">Bem vindo de volta</h1>
+  return (
+    <div className="flex justify-center items-center sm:h-[850px] md:h-[750px] m-5">
+      <form
+        className="flex flex-col justify-around gap-4 bg-white bg-opacity-90 w-[600px] h-full rounded-sm p-10"
+        action=""
+      >
+        <h1 className="text-lg text-center font-bold">Bem vindo de volta</h1>
 
-                <label className="text-base" htmlFor="email">E-mail:</label>
-                <input className="bg-black bg-opacity-15 p-1 w-full border-b-4 border-b-secondary-200 focus:outline outline-secondary-200 text-base" type="email" name="email" id="email" placeholder="exemplo@email.com" autoComplete="on" required />
+        <label className="text-base" htmlFor="email">
+          E-mail:
+        </label>
+        <input
+          className="bg-black bg-opacity-15 p-1 w-full border-b-4 border-b-secondary-200 focus:outline outline-secondary-200 text-base"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="exemplo@email.com"
+          autoComplete="on"
+          required
+        />
 
-                <PasswordInput />
+        <PasswordInput />
 
-                <KeepConected />
+        <KeepConected />
 
-                <input className="w-full bg-primary-200 hover:bg-secondary-200 text-white hover:text-secondary-300 transition-all hover:cursor-pointer py-4 rounded-md text-base font-bold" type="submit" value="Entrar" />
+        <input
+          className="w-full bg-primary-200 hover:bg-secondary-200 text-white hover:text-secondary-300 transition-all hover:cursor-pointer py-4 rounded-md text-base font-bold"
+          type="submit"
+          value="Entrar"
+        />
 
-                <div className="flex justify-between">
-                    <Link className="login-link" to="/create-account">criar conta</Link> <Link className="login-link" to="/forget-password">esqueci a senha</Link>
-                </div>
-
-                <div className="w-full my-5 flex flex-row justify-center items-center gap-5">
-                    <span className="bg-[#BBB] h-2 w-[40%] rounded-xl"></span>
-                    OU
-                    <span className="bg-[#BBB] h-2 w-[40%] rounded-xl"></span>
-                </div>
-
-                <div className="flex flex-row justify-center items-center gap-3">
-                    <span className="border-4 p-1 bg-white border-secondary-200 rounded-full"><Icon icon={"flat-color-icons:google"} width={"60px"} /></span>
-                    <span className="border-4 p-1 bg-white border-secondary-200 rounded-full"><Icon icon={"ic:baseline-facebook"} width={"60px"} /></span>
-                    <span className="border-4 p-1 bg-white border-secondary-200 rounded-full"><Icon icon={"ic:baseline-apple"} width={"60px"} /></span>
-                </div>
-            </form>
+        <div className="flex justify-between">
+          <Link className="login-link" to="/create-account">
+            criar conta
+          </Link>{" "}
+          <Link className="login-link" to="/forget-password">
+            esqueci a senha
+          </Link>
         </div>
-    );
+
+        <div className="w-full my-5 flex flex-row justify-center items-center gap-5">
+          <span className="bg-[#BBB] h-2 w-[40%] rounded-xl"></span>
+          OU
+          <span className="bg-[#BBB] h-2 w-[40%] rounded-xl"></span>
+        </div>
+
+        <div className="flex flex-row justify-center items-center gap-3">
+          <span className="border-4 p-1 bg-white border-secondary-200 rounded-full">
+            <Icon icon={"flat-color-icons:google"} width={"60px"} />
+          </span>
+          <span className="border-4 p-1 bg-white border-secondary-200 rounded-full">
+            <Icon icon={"ic:baseline-facebook"} width={"60px"} />
+          </span>
+          <span className="border-4 p-1 bg-white border-secondary-200 rounded-full">
+            <Icon icon={"ic:baseline-apple"} width={"60px"} />
+          </span>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
