eaf64db48d2e7626212411ec2f7f7cb3c0700a61
Marcos <msmarcondes10@gmail.com>
2024-11-01 16:47:30 -0300

    Refactor Planning component for improved readability and consistency

--- src/components/pages/Planning/Planning.jsx
+++ src/components/pages/Planning/Planning.jsx
@@ -1,16 +1,24 @@
-import Top from "./Top"
-import HeaderIntern from "../../Header/HeaderIntern"
-import AvatarFull from '../../Header/AvatarFull'
-function Planning(){
-    const [darkMode, setDarkMode] = useState(
-        document.body.classList.contains("dark")
-      );
-    return(
-        <>
-        <HeaderIntern iconActiveNumber={1} darkMode={darkMode} setDarkMode={setDarkMode}>
-            <AvatarFull name={'olaaa'} xp={'2000'} vcoins={'2000'}/>
-        </HeaderIntern><Top />
-        </>
-    )
+import Top from "./Top";
+import HeaderIntern from "../../Header/HeaderIntern";
+import AvatarFull from "../../Header/AvatarFull";
+import { useState } from "react";
+
+function Planning() {
+  const [darkMode, setDarkMode] = useState(
+    document.body.classList.contains("dark")
+  );
+
+  return (
+    <>
+      <HeaderIntern
+        iconActiveNumber={1}
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      >
+        <AvatarFull name={"olaaa"} xp={"2000"} vcoins={"2000"} />
+      </HeaderIntern>
+      <Top />
+    </>
+  );
 }
-export default Planning
+export default Planning;
