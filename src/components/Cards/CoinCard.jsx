47e4b53d9bc0878ecc60417f27ab8408b2529876
wesley <wesley.rochao@hotmail.com>
2024-11-22 20:47:15 -0300

    feat: update styling and layout for CoinCard, ProfileTags, and Top components

--- src/components/Cards/CoinCard.jsx
+++ src/components/Cards/CoinCard.jsx
@@ -2,11 +2,11 @@ function Card({ icon, title, text, color, background }) {
   return (
     <div
       style={background}
-      className={`${color} flex flex-col items-center justify-center rounded-md p-5 font-medium leading-[1] sm:h-[140px] sm:w-[160px]  lg:h-[100px] lg:w-[140px] text-white xl:h-[120px] xl:w-[160px] sm:px-14 lg:px-12 xl:px-14 2xl:h-[150px] 2xl:w-[200px] 2xl:px-20`}
+      className={`${color} flex flex-col items-center justify-center rounded-[16px] sm:rounded-md p-5 font-medium leading-[1] w-[70px] h-[60px] xs:w-[80px] xs:h-[75px] sm:h-[130px] sm:w-[130px]  lg:h-[100px] lg:w-[140px] text-white xl:h-[120px] xl:w-[160px] px-6 sm:px-12 lg:px-12 xl:px-14 2xl:h-[150px] 2xl:w-[200px] 2xl:px-20`}
     >
       <img src={icon} alt="" />
-      <h2 className="sm:text-[30px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px]">{title}</h2>
-      <p className="sm:text-[24] lg:text-[16px] xl:text-[20px] 2xl:text-[24px]">{text}</p>
+      <h2 className="text-[17px] xs:text-[20px] sm:text-[28px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px]">{title}</h2>
+      <p className="text-[12px] xs:text-[15px] sm:text-[22px] lg:text-[16px] xl:text-[20px] 2xl:text-[24px]">{text}</p>
     </div>
   );
 }
--- src/components/Cards/ProfileTags.jsx
+++ src/components/Cards/ProfileTags.jsx
@@ -1,7 +1,7 @@
 function Tags({ text }) {
   return (
     <>
-      <p className="m-2 rounded-[24px] bg-[rgba(31,31,31,0.20);] px-3 py-1.5 sm:text-[30px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px] text-[#252525] dark:bg-[#1F1F1F] dark:text-[#F5F5F480] whitespace-nowrap">
+      <p className="text- m-1 whitespace-nowrap rounded-[24px] bg-[rgba(31,31,31,0.20);] px-3 py-1.5 text-[#252525] xs:m-2 text-[18px] sm:text-[30px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px] dark:bg-[#1F1F1F] dark:text-[#F5F5F480]">
         {text}
       </p>
     </>
