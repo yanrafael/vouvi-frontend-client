5f1fc7ef59ed9a6bd775b2709d846c36f73dc06c
wesley <wesley.rochao@hotmail.com>
2024-11-22 20:06:56 -0300

    feat: enhance responsive design and styling for CoinCard, ProfileTags, and Top components

--- src/components/Cards/CoinCard.jsx
+++ src/components/Cards/CoinCard.jsx
@@ -2,11 +2,11 @@ function Card({ icon, title, text, color, background }) {
   return (
     <div
       style={background}
-      className={`${color} flex flex-col items-center justify-center rounded-md p-5 font-medium leading-[1] text-white xl:h-[110px] xl:w-[150px] xl:px-16 2xl:h-[150px] 2xl:w-[200px] 2xl:px-20`}
+      className={`${color} flex flex-col items-center justify-center rounded-md p-5 font-medium leading-[1] sm:h-[140px] sm:w-[160px]  lg:h-[100px] lg:w-[140px] text-white xl:h-[120px] xl:w-[160px] sm:px-14 lg:px-12 xl:px-14 2xl:h-[150px] 2xl:w-[200px] 2xl:px-20`}
     >
       <img src={icon} alt="" />
-      <h2 className="xl:text-[26px] 2xl:text-[32px]">{title}</h2>
-      <p className="xl:text-[20px] 2xl:text-[24px]">{text}</p>
+      <h2 className="sm:text-[30px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px]">{title}</h2>
+      <p className="sm:text-[24] lg:text-[16px] xl:text-[20px] 2xl:text-[24px]">{text}</p>
     </div>
   );
 }
--- src/pages/profile/Top.jsx
+++ src/pages/profile/Top.jsx
@@ -12,37 +12,42 @@ function Top() {
   const progressionbar = 19;
 
   return (
-    <section className="m-auto mt-14 flex w-[90%] items-center border-b-2 border-[#252525] pb-16 dark:border-[#F5F5F442]">
+    <section className="m-auto flex flex-col lg:flex-row w-[90%] items-center border-b-2 border-[#252525] pb-16 mt-16 lg:mt-10 xl:mt-14 dark:border-[#F5F5F442]">
       <div className="flex flex-none">
-        <div className="xl:w-[300px] xl:h-[300px] 2xl:h-[350px] 2xl:w-[350px] rounded-full bg-[#7C20BE80]">
-          <img src={Avatar} alt="Avatar Finn"  title="Avatar Finn"/> 
+        <div className="rounded-full bg-[#7C20BE80] sm:h-[300px] sm:w-[300px] lg:h-[250px] lg:w-[250px] xl:h-[300px] xl:w-[300px] 2xl:h-[350px] 2xl:w-[350px]">
+          <img src={Avatar} alt="Avatar Finn" title="Avatar Finn" />
         </div>
       </div>
-      <div className="ml-14 flex w-[90%] flex-col justify-center gap-2 font-medium text-[#252525] dark:text-white">
-        <h1 className="xl:text-base 2xl:text-lg">Ana carolina</h1>
-        <div className="bg-blue-500 w-[70%] rounded-md bg-[#014085] px-6">
-          <div className="flex justify-between text-right xl:text-[26px] 2xl:text-[32px] font-semibold">
+      <div className="flex w-full lg:w-[90%] flex-col items-center lg:items-stretch justify-center gap-2 font-medium text-[#252525] lg:ml-10 xl:ml-12 2xl:ml-14 dark:text-white">
+        <h1 className="sm:text-[40px] lg:text-[30px] xl:text-base 2xl:text-lg">
+          Ana carolina
+        </h1>
+        <div className="bg-blue-500 w-full lg:w-[80%] mt-14 lg:mt-0 rounded-md bg-[#014085] px-6">
+          <div className="flex justify-between text-right font-semibold sm:text-[30px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px]">
             <h2 className="text-white">Nivel</h2>
             <h2 className="text-secondary-200">Faltam 200XP</h2>
           </div>
-          <div className="m-auto mb-2 flex h-[10px] w-[100%] rounded-md bg-[#F5F5F480]" >
+          <div className="m-auto mb-2 flex h-[10px] w-[100%] rounded-md bg-[#F5F5F480]">
             <div
               style={{ width: `${progressionbar}%` }}
-              className={`rounded-md bg-white`} 
+              className={`rounded-md bg-white`}
             ></div>
           </div>
         </div>
-        <div className="flex w-4/6 flex-wrap font-medium" >
+        <div className="flex w-full lg:w-5/6 flex-wrap font-medium">
           <ProfileTags text={"Sessão de investimentos"} />
           <ProfileTags text={"15 Amigos"} />
         </div>
       </div>
-      <hr className="relative 2xl:right-[6.5%] h-96 w-[2px] bg-[#252525] dark:bg-[#F5F5F442]" />
-      <div className="flex flex-col justify-center"  title="ícone da vouvi, chamado Vcoins com uma estrela em cima  ">
-        <div className="flex">
+      <hr className="relative lg:right-[4.5%] m-4 lg:m-0 w-full lg:w-[2px] bg-[#252525] h-[2px] lg:h-72 xl:h-96 dark:bg-[#F5F5F442]" />
+      <div
+        className="flex lg:flex-col items-end lg:items-stretch gap-3 lg:gap-0"
+        title="ícone da vouvi, chamado Vcoins com uma estrela em cima  "
+      >
+        <div className="flex gap-3 lg:gap-0">
           <CoinCard
             color={
-              "mr-10 bg-[linear-gradient(180deg,rgba(255,77,0,0.80)_0%,rgba(255,216,46,0.80)_100%);]"
+              "lg:mr-5 xl:mr-8 2xl:mr-10 bg-[linear-gradient(180deg,rgba(255,77,0,0.80)_0%,rgba(255,216,46,0.80)_100%);]"
             }
             icon={Vicon}
             title={"928"}
@@ -56,11 +61,13 @@ function Top() {
             text={"emblemas"}
           />
         </div>
-        <div className="mt-4 flex justify-center rounded-md bg-[#14A90033] px-4 py-2 xl:text-[26px] 2xl:text-[32px] font-bold text-[#14A900]">
-          Sequência: 23 dias
-        </div>
-        <div className="mt-4 flex justify-center rounded-md bg-[#7C20BE33] px-4 py-2 xl:text-[26px] 2xl:text-[32px] font-bold text-primary-200">
-          Especialista em economia
+        <div className="flex flex-col">
+          <div className="mt-4 flex justify-center rounded-md bg-[#14A90033] px-4 py-2 font-bold text-[#14A900] md:text-[28px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px]">
+            Sequência: 23 dias
+          </div>
+          <div className="mt-4 flex justify-center rounded-md bg-[#7C20BE33] px-4 py-2 font-bold text-primary-200 md:text-[28px] lg:text-[21px] xl:text-[26px] 2xl:text-[32px]">
+            Especialista em economia
+          </div>
         </div>
       </div>
     </section>
