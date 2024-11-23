bb8c22d5e85cc9b50c4425807e015825f168305a
wesley <wesley.rochao@hotmail.com>
2024-11-22 22:04:17 -0300

    fix

--- src/pages/profile/Low.jsx
+++ src/pages/profile/Low.jsx
@@ -2,7 +2,7 @@ import Friends from "../../components/Cards/FriendCards";
 import { Icon } from "@iconify/react";
 function Low({ friends, openModal, openModal2 }) {
   return (
-    <section className="m-auto lg:mt-40 xl:mt-20 w-11/12 text-[40px] font-medium dark:text-white">
+    <section className="m-auto mt-20 w-11/12 text-[40px] font-medium dark:text-white">
       <div className="group flex flex-col rounded-md bg-[rgba(124,32,190,0.50);] p-5 transition-colors duration-500 hover:bg-primary-200 dark:bg-[#1e1e1e] dark:hover:bg-[#282828]">
         <div className="flex justify-between text-[#471650] group-hover:text-white dark:text-white">
           <h1 className="transition-colors duration-500">Amigos: {friends}</h1>
