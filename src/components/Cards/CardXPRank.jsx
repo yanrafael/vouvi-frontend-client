8e2d47587adad73621b178c9bfcc3585ea1d8af6
vitoria2caroline <Vcaroline.neves@gmail.com>
2024-11-24 17:25:19 -0300

    atualização
    ...

--- src/components/Cards/CardXPRank.jsx
+++ src/components/Cards/CardXPRank.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = (props) =>{
     return(
-      <div className="flex justify-between items-center lg:text-md bg-card p-2  rounded-[20px]  text-primary-200  bg-primary-200 dark:bg-[#3D3D3D] md:h-14 md:w-[330px] lg:w-[700px] w-[300px] h-10  md:ml-8 lg:ml-20 -mt-1 mb-2 ml-5"> 
+      <div className="flex justify-between items-center lg:text-md bg-card p-2  rounded-[20px]  text-primary-200  bg-primary-200 dark:bg-[#3D3D3D] md:h-14 md:w-[330px] lg:w-[700px] w-[300px] h-10  md:ml-8 lg:ml-28 -mt-1 mb-2 ml-5 lg:items-center"> 
         <div className="flex items-center ">
         <span className="mr-2 flex items-center justify-center lg:w-12 lg:h-12 rounded-full lg:text-[24px] bg-[#DFDFDE] dark:bg-black dark:text-white md:w-9 md:h-9 md:mx-2 md:text-[20px]  w-8 h-8 text-[20px]"  >{props.posicao}°</span>
           <div className="lg:text-base font-medium text-[#DFDFDE] dark:text-[#B1B1B1] md:text-[20px] text-[14px]">{props.nome}</div>
