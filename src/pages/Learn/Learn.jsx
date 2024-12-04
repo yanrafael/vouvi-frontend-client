b32c480b8f0a1a5533bc3664b9a36abcc4621c16
Marcos <msmarcondes10@gmail.com>
2024-12-04 16:23:42 -0300

    feat: add nextArrow SVG component to Learn page for enhanced navigation

--- src/pages/Learn/Learn.jsx
+++ src/pages/Learn/Learn.jsx
@@ -114,6 +114,22 @@ function Learn() {
     },
   ];
 
+  const nextArrow = (
+    <svg
+      width="49"
+      height="84"
+      viewBox="0 0 49 84"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="group absolute right-8 hidden cursor-pointer lg:block"
+    >
+      <path
+        d="M2.08181 81.7755C0.910111 80.6035 0.251889 79.0141 0.251889 77.3568C0.251889 75.6995 0.910111 74.1101 2.08181 72.938L33.0193 42.0005L2.08181 11.063C0.943324 9.88428 0.313357 8.30551 0.327598 6.66678C0.341838 5.02805 0.999145 3.46048 2.15794 2.30167C3.31675 1.14287 4.88433 0.485569 6.52306 0.471333C8.16178 0.457089 9.74055 1.08706 10.9193 2.22554L46.2756 37.5818C47.4473 38.7538 48.1055 40.3433 48.1055 42.0005C48.1055 43.6578 47.4473 45.2472 46.2756 46.4193L10.9193 81.7755C9.74725 82.9472 8.15783 83.6055 6.50055 83.6055C4.84328 83.6055 3.25385 82.9472 2.08181 81.7755Z"
+        className="fill-secondary-400 transition-all duration-200 group-hover:fill-secondary-200 dark:fill-white"
+      />
+    </svg>
+  );
+
   return (
     <>
       <Header>
@@ -142,6 +158,8 @@ function Learn() {
             }
           />
         </div>
+
+        {nextArrow}
 
         <LifeMeter lives={3} />
       </main>
